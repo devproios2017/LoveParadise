e67ee23cd9e4e694b010ffc8babe4ac4974c76a7
ThanhDat <phithanhdathd@gmail.com>
2017-06-08 20:21:26 +0700

    initial commit

---
+++ LoveParadise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveParadise
+//
+//  Created by Mac on 6/8/17.
+//  Copyright © 2017 DHT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // i just created it just 4 minute ago
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
