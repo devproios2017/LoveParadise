871b70e0bb86bbe446ca63e7ee1f67aa929ac44c
duchieuna <duchieuna@gmail.com>
2017-07-04 16:37:53 +0700

    tableview

---
+++ LoveParadise/ProductsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsTableViewCell.swift
+//  LoveParadise
+//
+//  Created by Hieupd on 7/4/17.
+//  Copyright © 2017 DHT. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
